7fc45e0dbcd38a893d953f2b1c6a10d4de71e445
Víctor Romero <46640258+IOhacker@users.noreply.github.com>
2021-10-15 23:41:06 -0500

    Create 01.java_installation.sh

---
+++ scripts/01.java_installation.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+sudo apt-get -q update
+sudo apt-get -yq install gnupg curl 
+
+# add Azul's public key
+sudo apt-key adv \
+  --keyserver hkp://keyserver.ubuntu.com:80 \
+  --recv-keys 0xB1998361219BD9C9
+
+# download and install the package that adds 
+# the Azul APT repository to the list of sources 
+curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-2_all.deb
+
+# install the package
+sudo apt-get install ./zulu-repo_1.0.0-2_all.deb
+
+# update the package sources
+sudo apt-get update
+
+# install Azul Zulu JDK 11
+sudo apt-get install zulu11-jdk
+
+#Verify Java Version
+java -version
