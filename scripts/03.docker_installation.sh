ebd2fa0d5b747368776c7ad5b13804257ee68464
Víctor Romero <46640258+IOhacker@users.noreply.github.com>
2021-10-16 01:04:40 -0500

    Update 03.docker_installation.sh

--- scripts/03.docker_installation.sh
+++ scripts/03.docker_installation.sh
@@ -37,7 +37,7 @@ sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
 #Run this command to download the current stable release of Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname-s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 #Apply executable permissions to the binary
 sudo chmod +x /usr/local/bin/docker-compose
